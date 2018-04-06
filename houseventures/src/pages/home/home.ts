908108fd3ce86677b9154ff761165b930b6dbb3c
Roderick Smith <smithroderick17@gmail.com>
2018-04-05 23:06:58 -0500

    Started api work

--- houseventures/src/pages/home/home.ts
+++ houseventures/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  coins: any;
+
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
+
+  ionViewDidLoad() {
+    console.log('The home page has loaded')
+    this.http.get('https://min-api.cryptocompare.com/data/top/volumes?tsym=USD&limit=20').subscribe((res: res) => {
+    console.log(res)
+    this.coins = res.Data;
+    console.log(this.coins);
+    })
+  }
+
+  navigateToCoinDetailsPage(coin) {
+    console.log(coin);
+    console.log('Navigating to appliance details page.');
+    this.navCtrl.push('CoinDetailsPage', {'coin': coin});
+    }
+
+}
+export interface res {
+  Data: any;
 
 }
