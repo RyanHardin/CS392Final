48cc22c8049516440eb9432e726bb8f553f0cf36
ryanhardin <rhardin23.rh@gmail.com>
2018-04-20 01:40:54 -0500

    made changes to the icon and splash screen

--- cryptoview/src/pages/home/home.ts
+++ cryptoview/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @Component({
   selector: 'page-home',
@@ -23,14 +25,14 @@ export class HomePage {
         for (var key in coins) {
           //console.log(key);
           if (coins.hasOwnProperty(key)) {
-              //console.log(key + " -> " + coins[key]);
-              list[key] = coins[key];
+            //console.log(key + " -> " + coins[key]);
+            list[key] = coins[key];
           }
 
-      }
+        }
         this.coins = list;
-        //console.log(this.coins);
-       })
+        console.log(this.coins);
+      })
       console.log('Async operation has ended');
       refresher.complete();
     }, 2000);
@@ -40,56 +42,35 @@ export class HomePage {
     //this.initializedItems();
     let list = [];
     this.ApiProvider.getAllCoins().subscribe((res: res) => {
-    //console.log(res.Data);
-    let coins = res.Data;
-    //console.log(coins);
-    for (var key in coins) {
-      //console.log(key);
-      if (coins.hasOwnProperty(key)) {
+      //console.log(res.Data);
+      let coins = res.Data;
+      //console.log(coins);
+      for (var key in coins) {
+        //console.log(key);
+        if (coins.hasOwnProperty(key)) {
           //console.log(coins[key]);
           list[key] = coins[key];
+          console.log(coins[key].Name);
+        }
       }
-  }
-  this.coins = list;
-  console.log(this.coins);
-  this.gotCoins = true;
-   })
-   //console.log(this.coins);
+      this.coins = list;
+      //console.log(this.coins);
+      this.gotCoins = true;
+    })
+    //console.log(this.coins);
   }
 
   ionViewDidLoad() {
     //console.log(this.coins);
-    //this.gotCoins = true;
+    this.gotCoins = true;
     //console.log(this.coins['BTC']);
   }
 
   navigateToCoinDetailsPage(coin) {
     //console.log(coin);
     //console.log('Navigating to appliance details page.');
-    this.navCtrl.push('CoinDetailsPage', {'coin': coin});
-    }
-  getItems(event) {
-
+    this.navCtrl.push('CoinDetailsPage', { 'coin': coin });
   }
-
-    /**initializedItems(){
-    this.items = this.coins.filter(function( obj ) {
-        return obj.name;
-      });
-
-  }
-
-    getItems(ev: any){
-      this.initializedItems();
-
-      let val = ev.target.value;
-
-      if (val && val.trim() != '') {
-        this.items = this.items.filter((item) => {
-          return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-        })
-      }
-    }**/
 
 }
 export interface res {
